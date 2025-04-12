8d86eeb12a004eff42f6e2474d4df7abd85e19e0
Priyansh28704 <priyanshupatidar28704@gmail.com>
2025-04-13 01:36:16 +0530

    a

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import logout
+from django.contrib.auth import logout,login,authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
 from .forms import VendorForm
 
@@ -14,6 +15,27 @@ def register_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('homepage') # Apne homepage ka URL name se replace karo
+            else:
+                # Invalid login credentials
+                pass
+        else:
+            # Invalid form
+            pass
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
 
 def logout_view(request):
     logout(request)
