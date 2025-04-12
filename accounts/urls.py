ba52610ad50bc59a7f3224503e463ba70f847801
Priyansh28704 <priyanshupatidar28704@gmail.com>
2025-04-13 00:40:50 +0530

    a

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ from .views import vendor_register
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', views.logout_view, name='logout'),
+   # path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
     path('vendor-register/', vendor_register, name='vendor_register'),
 
